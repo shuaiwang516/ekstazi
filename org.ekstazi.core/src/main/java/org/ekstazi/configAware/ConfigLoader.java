d299c6960c68d5f2a79f8ca5db78a88f6281a59a
shuaiwang516 <shuaiwang516@gmail.com>
2022-01-23 16:07:51 -0600

    keep empty value for debug

--- org.ekstazi.core/src/main/java/org/ekstazi/configAware/ConfigLoader.java
+++ org.ekstazi.core/src/main/java/org/ekstazi/configAware/ConfigLoader.java
@@ -127,7 +127,7 @@ public class ConfigLoader {
 
                 // Multiple configuration files may have duplicated settings. We choose the last one as the final value (Overwrite)
                 // This is the same idea as some real-world software like Hadoop.
-                if (!Objects.equals(configName, "") && !Objects.equals(configValue, "")) {
+                if (!Objects.equals(configName, "")) {
                     sExercisedConfigMap.put(replaceBlank(configName), replaceBlank(configValue));
                 }
                 //System.out.println(configName + " , " + configValue);
