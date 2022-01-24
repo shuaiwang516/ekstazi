bfccd47b3d8ed04f49dc1d54d712e0456fe4de0c
shuaiwang516 <shuaiwang516@gmail.com>
2022-01-23 21:44:29 -0600

    remove formatting for debug

--- org.ekstazi.core/src/main/java/org/ekstazi/configAware/ConfigLoader.java
+++ org.ekstazi.core/src/main/java/org/ekstazi/configAware/ConfigLoader.java
@@ -128,7 +128,8 @@ public class ConfigLoader {
                 // Multiple configuration files may have duplicated settings. We choose the last one as the final value (Overwrite)
                 // This is the same idea as some real-world software like Hadoop.
                 if (!Objects.equals(configName, "")) {
-                    sExercisedConfigMap.put(replaceBlank(configName), replaceBlank(configValue));
+                    //sExercisedConfigMap.put(replaceBlank(configName), replaceBlank(configValue));
+                    sExercisedConfigMap.put(configName, configValue);
                 }
                 //System.out.println(configName + " , " + configValue);
             }
